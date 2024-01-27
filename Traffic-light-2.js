ca1a7a493cda85e8dee56923d682dd43ed64bcc3
AmalSalah <amalsalah@AmalSalahs-MacBook-Air.local>
2024-01-27 22:28:44 +0100

    created Traffic-light-2.js markdown file

--- Traffic-light-2.js
+++ Traffic-light-2.js
@@ -1,1 +1,18 @@
-
+"use strict";
+const trafficLight = {
+  possibleStates: ["green", "orange", "red"],
+  stateIndex: 0,
+};
+let cycle = 0;
+while (cycle < 2) {
+  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+  console.log("The traffic light is on", currentState);  
+}
+if (currentState === "green") {
+  trafficLight.stateIndex = "orange";
+} else if (currentState === "orange") {
+  trafficLight.stateIndex = "red";
+} else {
+ trafficLight.stateIndex = "green";
+  cycle++
+}
